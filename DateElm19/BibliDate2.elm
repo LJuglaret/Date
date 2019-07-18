276295a8d8931fcc86cd6716e39eef196322f01a
Laure Juglaret <ljuglaret@gmail.com>
2019-07-18 12:13:27 +0100

    Update BibliDate2.elm

--- DateElm19/BibliDate2.elm
+++ DateElm19/BibliDate2.elm
@@ -2,12 +2,7 @@ module BibliDate2 exposing (..)
 import Mois exposing (..)
 import Date2 exposing (..)
 
-import Html exposing (..)
-
-import Html.Events exposing (..)
-
-
-
+---     COMPARAISONS    ---
 
 mois1AvantMois2 : Mois -> Mois -> Bool
 mois1AvantMois2 mois1 mois2 = 
@@ -18,7 +13,6 @@ mois1AvantMois2 mois1 mois2 =
         if (numM1 <= numM2)
         then True
         else False
-
 
 
 compD1D2 : Maybe  Date2 -> Maybe  Date2 -> Maybe Order
@@ -39,9 +33,6 @@ compD1D2 date1c date2c =
                             GT -> Just GT
                     else Just GT
         (_,_) -> Nothing
-
----
-
 
 sup : Maybe Date2 -> Maybe Date2 -> Result String Bool  
 sup d1 d2 = 
@@ -108,99 +99,9 @@ supOuEgal d1 d2 =
             (_ , _)            -> Err "format date incorrect" 
 
 
----
+---     OPERATIONS SUR LES DATES    --- 
 
---- Jour De La Semaine 
-
-type Jour = Lundi
-            |Mardi
-            |Mercredi
-            |Jeudi
-            |Vendredi
-            |Samedi
-            |Dimanche
-
-datetest : Maybe Date2
-datetest = newDate {jour = 11, mois = Septembre, annee = 2018}
-nomJourtest : Jour
-nomJourtest = Mardi
-
-jourDeLaSemaine : Maybe Date2 -> Int
-jourDeLaSemaine dateM =  
-    case dateM of 
-        Just date -> 
-                let 
-                        d = (dateVersAlias date).jour
-                        mois = (dateVersAlias date).mois 
-                        m = (listeInfoMois mois).numeroDuMois
-                        y = (dateVersAlias date).annee  
-                        z = y - 1
-                in
-                    if (m >= 3)
-                    then modBy (  23 * m//9 + d + 4 + y + y//4 - y//100 + y//400 - 2 ) 7
-                    else modBy ( 23 * m//9 + d + 4 + y + z//4 - z//100 + z//400 ) 7
-        Nothing  -> -1
-
-date3 = newDate{jour = 11, mois = Septembre, annee = 2018}
-j : Jour
-j = numeroEnJour (jourDeLaSemaine date3)
-
-numeroEnJour : Int -> Jour  
-numeroEnJour i = 
-    if (i==0) then Dimanche
-    else if (i == 1) then Lundi
-    else if (i == 2) then Mardi
-    else if (i == 3) then Mercredi
-    else if (i == 4) then Jeudi
-    else if (i == 5) then Vendredi
-    else  Samedi
-----
-
-fromJour : Jour -> String 
-fromJour jourj = "jourj"
-
---- affichage 
-
-aff2 : Maybe Date2 -> String 
-aff2 dateM = 
-    case dateM of 
-        Just date -> let 
-                        d = (dateVersAlias date).jour
-                  
-                in  String.fromInt d
-        Nothing -> ""
-
-
---sortWith : (a -> a -> Order) -> List a -> List a
-
-compD1D2Correct : List (Maybe Date2) -> List Date2
-compD1D2Correct l0 = case l0 of 
-        x :: l -> 
-            case x of 
-                Just x0 -> x0 :: (compD1D2Correct l)
-                Nothing  ->  (compD1D2Correct l)
-        _ -> []
-avancement : Maybe Date2 -> Int
-avancement dM =
-    case dM of   
-        Just d -> 
-            let
-                annee = (dateVersAlias d).annee
-                mois = (dateVersAlias d).mois
-                numMois =  (listeInfoMois mois).numeroDuMois
-                z = List.map (\y -> (listeInfoMois (complete y correspondances)).nombreDeJours) (List.range 1 (numMois - 1 ) )
-            in ((List.sum z) + (dateVersAlias d).jour)
-        _ -> 0
-
---   31 decembre - avancement date1 + avancement date2
-ecartD1D2 :  Maybe Date2 ->Maybe Date2 -> Int
-ecartD1D2 date1 date2  = 
-    case (date1, date2) of 
-        (Just d1, Just d2) -> 
-            let d3 = newDate{jour = 31, mois = Decembre, annee = (dateVersAlias d1).annee}
-            in  ((avancement d3) -  (avancement date1)) + (avancement date2)
-        (_ ,_) ->0
-
+---     ANNEE      ---
 
 anneePlus : Maybe Date2 -> Maybe Date2
 anneePlus dateM = 
@@ -220,6 +121,34 @@ anneeMoins dateM =
                 annee0 = (dateVersAlias  date).annee
             in newDate{jour = jour0, mois = mois0, annee = annee0 - 1}
     )
+
+
+---     MOIS    ---
+
+moisPrecedent :Mois -> Mois 
+moisPrecedent mois = 
+    let
+        moisActuel       = complete ( (listeInfoMois mois).numeroDuMois ) correspondances
+        moisActuelNumero = (listeInfoMois mois).numeroDuMois 
+    in
+        if moisActuelNumero == 1
+        then Decembre 
+        else  complete (moisActuelNumero  - 1 ) correspondances
+
+
+
+moisSuivant :Mois -> Mois 
+moisSuivant mois = 
+    let
+        moisActuel       = complete ( (listeInfoMois mois).numeroDuMois ) correspondances
+        moisActuelNumero = (listeInfoMois mois).numeroDuMois 
+    in
+        if moisActuelNumero == 12
+        then Janvier
+        else  complete (moisActuelNumero  + 1) correspondances
+
+
+
 
 moisPlus  : Maybe  Date2 -> Maybe Date2
 moisPlus dateM = 
@@ -246,120 +175,31 @@ moisMoins dateM =
                 else newDate{jour = 1, mois = moisPrecedent mois0, annee = annee0 }
     )
 
-estMardi : Maybe Date2 -> Bool
-estMardi dM = (numeroEnJour (jourDeLaSemaine dM) == Mardi )
-    
-f date =List.concat ( List.map (List.filter (\l -> (\y -> estMardi y) l )) (partage2 (joursMois2 date)))
-
---f date = List.map (List.filter (\l  -> String.contains "Mardi"  l )) (partage (joursMois date))
-meetupElm : Maybe Date2 -> Maybe Date2 
-meetupElm date =   
-    let 
-        aux : List (Maybe Date2 ) -> Int -> Maybe Date2 
-        aux l0 cpt = 
-            case l0 of 
-                x::l -> if (cpt /= 2 )
-                        then aux l (cpt + 1)
-                        else x
-                []  -> Nothing
-
-    in aux (f date ) 0
 
 
-premierLundi : Maybe Date2 -> Maybe Date2
-premierLundi dateM = 
+
+
+---     JOURS   ---
+
+jourPrecedent : Maybe Date2 -> Maybe Date2
+jourPrecedent dateM = 
     dateM |> Maybe.andThen ( \date ->
-             let acc date0 =
-                            if (numeroEnJour (jourDeLaSemaine date0) /= Lundi)
-                            then acc (jourSuivant date0)
-                            else date0
-                    
-            in acc (newDate {jour = 1, mois = (dateVersAlias date).mois, annee =(dateVersAlias date).annee}) 
+            let jour0  = (dateVersAlias  date).jour
+                mois0  = (dateVersAlias  date).mois
+                annee0 = (dateVersAlias  date).annee
+            in
+                if (jour0 == 1 && mois0 == (listeInfoMois Janvier).mois ) 
+                then newDate {jour = 31, mois = Decembre, annee = annee0 - 1}
+                else
+                    if ((jour0 == 1) && (estBissextile annee0))
+                    then newDate{jour = 29, mois = Fevrier, annee = annee0}
+                    else
+                        if (jour0 == 1)
+                        then  newDate {jour = (listeInfoMois (moisPrecedent mois0)).nombreDeJours, mois = moisPrecedent mois0, annee = annee0}
+                        else  newDate{jour = jour0 - 1 , mois = mois0, annee = annee0  }
+
     )
 
-dernierDimanche : Maybe Date2 -> Maybe Date2
-dernierDimanche  dateM = 
-    dateM |> Maybe.andThen ( \date ->
-             let acc date0 =
-                            if (numeroEnJour (jourDeLaSemaine date0) /= Dimanche)
-                            then acc (jourPrecedent date0)
-                            else date0
-                                
-            in acc (newDate {jour = (listeInfoMois (dateVersAlias date).mois).nombreDeJours, mois = (dateVersAlias date).mois, annee =(dateVersAlias date).annee}) 
-    )
-
------------ 
-
-joursMois2 : Maybe Date2 -> List (Maybe Date2)
-joursMois2  dateM  =  
-    case dateM of   
-        Just date -> 
-            let  
-                acc dateDebut debut n fin = 
-                    if (n > fin)
-                    then []
-                    else dateDebut :: (acc (jourSuivant dateDebut) (debut + 1 ) ( n + 1) fin)
-            in acc (newDate {jour = 1, mois = (dateVersAlias date).mois, annee =(dateVersAlias date).annee})  1 1 (listeInfoMois  (dateVersAlias date).mois ).nombreDeJours 
-        Nothing -> []
-
-completeAvant2 : Maybe Date2 -> List (Maybe Date2)
-completeAvant2 dateM =
-    case dateM of   
-        Just date -> 
-            let premLundi = premierLundi dateM
-            in
-                let 
-                    jMoisAvant = (listeInfoMois  (moisPrecedent (dateVersAlias date).mois )).nombreDeJours 
-                    numLundi   = 
-                        case premLundi of 
-                            Just x -> (dateVersAlias x).jour 
-                            Nothing  -> 0
-                in
-                    let  
-                        acc dateDebut  n fin = 
-                            if (n > fin)
-                            then []
-                            else dateDebut :: (acc (jourSuivant dateDebut)  ( n + 1) fin) -- (toString (numeroEnJour (jourDeLaSemaine dateDebut))++ " " ++ (toString (dateVersAlias x).jour)):: (acc (jourSuivant dateDebut)  ( n + 1) fin)
-                    in acc (niemeJourAvant premLundi 7 ) ( jMoisAvant - (7 - numLundi ))  jMoisAvant 
-        Nothing -> []
-
-
-completeApres2 : Maybe Date2 -> List (Maybe Date2)
-completeApres2 dateM =
-    case dateM of   
-        Just date -> 
-            let dernDimanche = dernierDimanche dateM
-            in
-                let 
-                    jMois = (listeInfoMois   (dateVersAlias date).mois ).nombreDeJours 
-                    numDimanche   = 
-                        case dernDimanche of 
-                            Just x -> (dateVersAlias x).jour 
-                            Nothing  -> 0
-                in
-                    let  
-                        acc dateDebut  n fin = 
-                            if (n > fin)
-                            then []
-                            else dateDebut :: (acc (jourSuivant dateDebut)  ( n + 1) fin) 
-                    in acc (jourSuivant (newDate {jour = jMois, mois = (dateVersAlias date).mois, annee =(dateVersAlias date).annee})) 1 ( 7 - (jMois  - numDimanche ))
-        Nothing -> []
-
-
-
-partage2 : List (Maybe Date2) -> List(List (Maybe Date2)) 
-partage2 l0 = 
-    let aux l cpt =
-                    if (not (List.isEmpty l))
-                    then if (( modBy cpt 7 ) /= 0)
-                        then [(List.take 7 l)]++ (aux (List.drop 7 l) ( 1  + cpt))
-                        else (aux l ( 1  + cpt))
-                    else []
-    in aux l0 0 
-
---------------- pareil mais avec des monade 
-
--- rappel  : Maybe.andThen : ( a -> Maybe b  ) -> Maybe a -> Maybe b
 
 jourSuivant : Maybe Date2 -> Maybe Date2
 jourSuivant dateM = 
@@ -382,8 +222,13 @@ jourSuivant dateM =
 
 
 
-
-
+niemeJourAvant : Maybe Date2 -> Int -> Maybe Date2
+niemeJourAvant date n = 
+    if (n == 0) then date  
+    else  
+        if (n > 0)
+        then niemeJourAvant (jourPrecedent date ) (n - 1 )
+        else niemeJourApres ( jourSuivant date ) (-1 * n - 1)
 
 
 niemeJourApres : Maybe Date2 -> Int -> Maybe Date2
@@ -396,51 +241,57 @@ niemeJourApres date n =
 
 
 
+---     EXEMPLES    ---
 
-moisPrecedent :Mois -> Mois 
-moisPrecedent mois = 
-    let
-        moisActuel       = complete ( (listeInfoMois mois).numeroDuMois ) correspondances
-        moisActuelNumero = (listeInfoMois mois).numeroDuMois 
-    in
-        if moisActuelNumero == 1
-        then Decembre 
-        else  complete (moisActuelNumero  - 1 ) correspondances
+d0 = newDate  {jour  = 25 , mois = Mois.Septembre, annee = 2018}
+d01 = newDate  {jour  = 27 , mois = Mois.Janvier, annee = 2018}
+d03 = newDate{jour = 18, mois = Juillet , annee = 2019}
 
-moisSuivant :Mois -> Mois 
-moisSuivant mois = 
-    let
-        moisActuel       = complete ( (listeInfoMois mois).numeroDuMois ) correspondances
-        moisActuelNumero = (listeInfoMois mois).numeroDuMois 
-    in
-        if moisActuelNumero == 12
-        then Janvier
-        else  complete (moisActuelNumero  + 1) correspondances
 
-jourPrecedent : Maybe Date2 -> Maybe Date2
-jourPrecedent dateM = 
-    dateM |> Maybe.andThen ( \date ->
-            let jour0  = (dateVersAlias  date).jour
-                mois0  = (dateVersAlias  date).mois
-                annee0 = (dateVersAlias  date).annee
-            in
-                if (jour0 == 1 && mois0 == (listeInfoMois Janvier).mois ) 
-                then newDate {jour = 31, mois = Decembre, annee = annee0 - 1}
-                else
-                    if ((jour0 == 1) && (estBissextile annee0))
-                    then newDate{jour = 29, mois = Fevrier, annee = annee0}
-                    else
-                        if (jour0 == 1)
-                        then  newDate {jour = (listeInfoMois (moisPrecedent mois0)).nombreDeJours, mois = moisPrecedent mois0, annee = annee0}
-                        else  newDate{jour = jour0 - 1 , mois = mois0, annee = annee0  }
 
-    )
+---     AFFICHAGE   ---
 
-niemeJourAvant : Maybe Date2 -> Int -> Maybe Date2
-niemeJourAvant date n = 
-    if (n == 0) then date  
-    else  
-        if (n > 0)
-        then niemeJourAvant (jourPrecedent date ) (n - 1 )
-        else niemeJourApres ( jourSuivant date ) (-1 * n - 1)
+type Jour = Lundi
+            |Mardi
+            |Mercredi
+            |Jeudi
+            |Vendredi
+            |Samedi
+            |Dimanche
 
+jourDeLaSemaine : Maybe Date2 -> Int
+jourDeLaSemaine dateM =  
+    case dateM of 
+        Just date -> 
+                let 
+                        d = (dateVersAlias date).jour
+                        mois = (dateVersAlias date).mois 
+                        m = (listeInfoMois mois).numeroDuMois
+                        y = (dateVersAlias date).annee  
+                        z = y - 1
+                in
+                    if (m >= 3)
+                    then modBy 7 (  23 * m//9 + d + 4 + y + y//4 - y//100 + y//400 - 2 ) 
+                    else modBy 7 ( 23 * m//9 + d + 4 + y + z//4 - z//100 + z//400 ) 
+        Nothing  -> -1
+
+
+
+numeroEnJour : Int -> Jour  
+numeroEnJour i = 
+    if (i==0) then Dimanche
+    else if (i == 1) then Lundi
+    else if (i == 2) then Mardi
+    else if (i == 3) then Mercredi
+    else if (i == 4) then Jeudi
+    else if (i == 5) then Vendredi
+    else  Samedi
+
+affichage : Maybe Date2 -> String 
+affichage d =  
+    case d of 
+        Just dm -> Debug.toString((numeroEnJour (jourDeLaSemaine d)))++" "++ 
+                  Debug.toString(((dateVersAlias dm).jour))++ " "++
+                 Debug.toString((dateVersAlias dm).mois)++ " "++
+                  Debug.toString(((dateVersAlias dm).annee))
+        Nothing -> ""
